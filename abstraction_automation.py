73ddb0c7d5c8f90c437581dc3cf7b1ae4b877b92
MinTagg <ohht10004@gmail.com>
2024-04-21 03:54:05 +0900

    Add annotations

--- abstraction_automation.py
+++ abstraction_automation.py
@@ -9,15 +9,14 @@ def get_topics():
     for file_path in file_paths:
         with open(file_path, 'r', encoding='utf-8') as f:
             contents = f.read()
-        index = contents.find('완벽한 영상요약, 릴리스에이아이')
-        contents = contents[:index].replace('   ', '').split('\n\n')[:-1] # 뒤쪽 릴리스 광고 삭제 + 의미없는 띄워쓰기 삭제 + 문단별로 나눠서 리스트로 저장
-        # use filter function
+        index = contents.find('완벽한 영상요약, 릴리스에이아이') # 뒤쪽 릴리스 광고 삭제
+        contents = contents[:index].replace('   ', '').split('\n\n')[:-1] # 뒤쪽 릴리스 광고 삭제 + 의미없는 띄어쓰기 삭제 + 문단별로 나눠서 리스트로 저장
+        # filter 함수를 통해 관련있는 문단만 추출
         if FILTER_DEBUGGING:
             contents, debugging_history = filter(contents, debug = FILTER_DEBUGGING)
         else:
             contents = filter(contents, debug = FILTER_DEBUGGING)
-        
-        ## DELETE OUT OF CONTEXT TEXT USING GPT
+        # gpt_call 함수를 통해 문단을 카테고리로 분류
         response = gpt_call(contents, SYSTEM_SCRIPT)
         
         if not os.path.exists(f'{FOLDER_NAME}_category'):
@@ -58,15 +57,15 @@ def gpt_call(contents, system_script, gpt = 'gpt-4-0613'):
     return response.choices[0].message.content
 
 def filter(contents, debug = False):
-    temp_context = contents[0]
+    temp_context = contents[0] # 첫번째 제목을 context로 설정
     if debug:
         debugging_history = ''
-    for i in range(1, len(contents)):
+    for i in range(1, len(contents)): # 한 문단씩 읽어가면서 context와 주제를 바탕으로 관련있는지 확인
         gpt_input = f"<Main topic>: {SEARCH_NAME}\n<Context>: {temp_context}\n<Target>: {contents[i]}"
         response = gpt_call(contents= gpt_input, system_script= FILTER_PROMPT)
-        if 'True' in response:
+        if 'True' in response: # 관련이 있는 경우 context에 추가
             temp_context += '\n' + contents[i]
-        elif 'False' in response and debug:
+        elif 'False' in response and debug: # 관련이 없는 경우 디버깅 히스토리에 추가
             debugging_history += f"{contents[i]}\n"
     if debug:
         return temp_context, debugging_history
