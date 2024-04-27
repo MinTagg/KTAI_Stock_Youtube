cd7727be8e2433e021e2352d3ad7d5ea45ef6b97
MinTagg <ohht10004@gmail.com>
2024-04-27 15:36:30 +0900

    add retry logic to the function

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from crawling import get_summary_with_query
 from config import *
 
 if __name__ == '__main__':
-    get_summary_with_query(SEARCH_NAME, NUM_OF_VIDEO)
+    state = True
+    while state:
+        try:
+            get_summary_with_query(SEARCH_NAME, NUM_OF_VIDEO)
+            state = False
+        except:
+            continue
     get_topics()
